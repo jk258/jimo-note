ef78cfdb7cc9649ae7206f21dfc6129207fdc99a
renguowei <18135185792@163.com>
2023-10-08 15:43:03 +0800

    搜索

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,21 @@ const config = {
 			}),
 		],
 	],
-
+	themes: [
+		// ... Your other themes.
+		[
+			// @ts-ignore
+			require.resolve('@easyops-cn/docusaurus-search-local'),
+			/** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+			// @ts-ignore
+			({
+				// ... Your options.
+				// `hashed` is recommended as long-term-cache of index file is possible.
+				hashed: true,
+				language: ['en', 'zh'],
+			}),
+		],
+	],
 	themeConfig:
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
