13efce2ab5bec833716981f35f179e242a91508e
renguowei <18135185792@163.com>
2023-10-08 14:23:59 +0800

    pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -153,43 +153,12 @@ const config = {
 					{
 						tagName: 'link',
 						rel: 'manifest',
-						href: '/manifest.json',
+						href: '/manifest.json', // your PWA manifest
 					},
 					{
 						tagName: 'meta',
 						name: 'theme-color',
 						content: 'rgb(37, 194, 160)',
-					},
-					{
-						tagName: 'meta',
-						name: 'apple-mobile-web-app-capable',
-						content: 'yes',
-					},
-					{
-						tagName: 'meta',
-						name: 'apple-mobile-web-app-status-bar-style',
-						content: '#000',
-					},
-					{
-						tagName: 'link',
-						rel: 'apple-touch-icon',
-						href: '/img/docusaurus.png',
-					},
-					{
-						tagName: 'link',
-						rel: 'mask-icon',
-						href: '/img/docusaurus.svg',
-						color: 'rgb(37, 194, 160)',
-					},
-					{
-						tagName: 'meta',
-						name: 'msapplication-TileImage',
-						content: '/img/docusaurus.png',
-					},
-					{
-						tagName: 'meta',
-						name: 'msapplication-TileColor',
-						content: '#000',
 					},
 				],
 			},
