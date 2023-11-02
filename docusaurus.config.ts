9aac3272785a7a0c8bce4ffc0996751181d87706
renguowei <18135185792@163.com>
2023-11-02 14:09:52 +0800

    升级docusaurus v3

---
+++ docusaurus.config.ts
@@ -0,0 +1,165 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+// import lightCodeTheme from 'prism-react-renderer/themes/github'
+// import darkCodeTheme from 'prism-react-renderer/themes/dracula'
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
+
+const config: Config = {
+	title: '即墨',
+	tagline: '即墨的个人文档',
+	favicon: 'img/favicon.ico',
+
+	// Set the production url of your site here
+	url: 'https://your-docusaurus-test-site.com',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/jimo-note/',
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'jk258', // Usually your GitHub org/user name.
+	projectName: 'jimo-note', // Usually your repo name.
+
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
+
+	presets: [
+		[
+			'classic',
+			{
+				docs: {
+					sidebarPath: './sidebars.ts',
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				blog: {
+					showReadingTime: true,
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			} satisfies Preset.Options,
+		],
+	],
+	themeConfig: {
+		// Replace with your project's social card
+		image: 'img/docusaurus-social-card.jpg',
+		navbar: {
+			title: '即墨',
+			logo: {
+				alt: '即墨 Logo',
+				src: 'img/logo.svg',
+			},
+			items: [
+				{
+					type: 'docSidebar',
+					sidebarId: 'webSidebar',
+					position: 'left',
+					label: '前端',
+				},
+				{
+					type: 'docSidebar',
+					sidebarId: 'pythonSidebar',
+					position: 'left',
+					label: 'python',
+				},
+				// { to: "/blog", label: "Blog", position: "left" },
+				{
+					href: 'https://github.com/jk258/jimo-note',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'docs',
+					items: [
+						{
+							label: '前端',
+							to: '/docs/category/构建工具',
+						},
+						{
+							label: 'python',
+							to: '/docs/python/基础教程/1.1基本语法',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Stack Overflow',
+							href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+						},
+						{
+							label: 'Discord',
+							href: 'https://discordapp.com/invite/docusaurus',
+						},
+						{
+							label: 'Twitter',
+							href: 'https://twitter.com/docusaurus',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'GitHub',
+							href: 'https://github.com/jk258/jimo-note',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
+	plugins: [
+		[
+			'@docusaurus/plugin-pwa',
+			{
+				debug: true,
+				offlineModeActivationStrategies: ['appInstalled', 'standalone', 'queryString'],
+				pwaHead: [
+					{
+						tagName: 'link',
+						rel: 'icon',
+						href: '/img/docusaurus.png',
+					},
+					{
+						tagName: 'link',
+						rel: 'manifest',
+						href: '/manifest.json', // your PWA manifest
+					},
+					{
+						tagName: 'meta',
+						name: 'theme-color',
+						content: 'rgb(37, 194, 160)',
+					},
+				],
+			},
+		],
+	],
+}
+export default config
